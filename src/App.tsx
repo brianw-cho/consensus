740634867f1d54cc0a8fdc47cbb04fd11e508406
Brian Grigore <73979278+Brian-GG@users.noreply.github.com>
2023-01-21 17:30:40 -0500

    added google scraping functionality
    it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BiArrowBack } from 'react-icons/bi';
 import Button from './components/button/Button';
+import ReviewService from './services/ReviewsService';
 
 function App() {
+  const reviewService = ReviewService();
   return (
     <div className="App">
       <div className="bg">
         <div className="header">
           <Button 
-            onClick={() => {}}
+            onClick={() => {
+              const _reviews = reviewService.getReviewsFromID("4");
+            }}
             child={<BiArrowBack />}
             width="30px"
             height="30px"
--- src/services/ReviewsService.ts
+++ src/services/ReviewsService.ts
@@ -1,6 +1,82 @@
-const ReviewService = () => {
 
-  return {};
+const ReviewService = () => {
+ 
+
+
+
+  let latitude = 0
+  let longitude = 0
+ 
+  const getReviews = () => {
+    navigator.geolocation.getCurrentPosition(function(position)
+    {
+      latitude = position.coords.latitude
+      longitude = position.coords.longitude
+    }
+    )
+    
+    
+    
+    let hotel_string = "ritz-carlton-hotel"
+    let zoom = 15.1
+    let ll_string = `@${latitude},${longitude},${zoom}z`
+    let apiKey_string = '6a461a33a2f5a9b53c15e06349a270bc1df302df4434f9d18e78deda5703eee9'    
+
+    fetch(`https://serpapi.com/search.json?engine=google_maps&q=${hotel_string}&ll=${ll_string}&type=search&api_key=${apiKey_string}`, {method: 'GET',
+    // mode: 'no-cors',
+    })
+    .then(response => response.json())
+    .then(function(result){})
+    .catch(error => console.log('error', error))
+
+
+
+
+
+
+    fetch(`https://serpapi.com/search.json?engine=google_maps&q=${hotel_string}&ll=${ll_string}&type=search&api_key=${apiKey_string}`, {method: 'GET',
+    // mode: 'no-cors',
+    })
+    .then(response => response.json())
+    .then(function(result){
+
+
+        })
+    .catch(error => console.log('error', error))
+ 
+  };
+
+
+  const getReviewsFromID = (dataID : String) => {
+    let reviews: Array<string> = [];
+    dataID = "0x89c259af336b3341:0xa4969e07ce3108de"
+    let apiKey_string = '6a461a33a2f5a9b53c15e06349a270bc1df302df4434f9d18e78deda5703eee9'    
+
+    fetch(`https://serpapi.com/search.json?engine=google_maps_reviews&data_id=${dataID}&sort_by=newestFirst&api_key=${apiKey_string}`, {method: 'GET',
+    })
+    .then(response => response.json())
+    .then(function(result){
+      for(let i = 0; i<result["reviews"].length; i++){
+        console.log(result["reviews"][i]["snippet"])
+        reviews[i] = result["reviews"][i]["snippet"]
+      }
+  
+    })
+    .catch(error => console.log('error', error))
+  
+  
+  return reviews
+  
+  }
+
+  return {
+    getReviews,
+    getReviewsFromID
+  };
 };
+
+
+
+
 
 export default ReviewService;
